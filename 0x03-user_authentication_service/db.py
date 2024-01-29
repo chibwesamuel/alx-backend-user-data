ef028b53ee4110293904de166ea597d6276bbfe2
Samuel Mukosa Chibwe <102081659+chibwesamuel@users.noreply.github.com>
2024-01-29 10:19:05 +0200

    Update 1 for db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
-
 """
-DB moduLe
+Implements the different requirements for DB module
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from typing import Any
 
 from user import Base, User
 
 class DB:
     """
-    DB class
+    DB class representing a database session
     """
 
     def __init__(self) -> None:
@@ -32,11 +34,40 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: str) -> User:
+    def _add_user(self, email: str, hashed_password: str) -> User:
         """
         Add a new user to the database
+
+        Args:
+            email (str): The email of the user
+            hashed_password (str): The hashed password of the user
+
+        Returns:
+            User: The newly created User object
         """
         new_user = User(email=email, hashed_password=hashed_password)
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs: Any) -> User:
+        """
+        Find a user by the given criteria
+
+        Args:
+            **kwargs: Arbitrary keyword arguments for filtering the query
+
+        Returns:
+            User: The first user found matching the given criteria
+
+        Raises:
+            NoResultFound: If no user is found matching the given criteria
+            InvalidRequestError: If wrong query arguments are passed
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if user is None:
+                raise NoResultFound
+            return user
+        except InvalidRequestError:
+            raise
