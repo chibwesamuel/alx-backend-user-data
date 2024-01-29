a96f28dc6690443be1ca2d7a7bb22f4cffff9cc1
chibwesamuel <smchibwe@yahoo.com>
2024-01-29 10:06:00 +0200

    New updates

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 """
-DB module
+DB moduLe
 """
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-User module
+Creates a SQLAlchemy model named User for a database table
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
@@ -9,12 +9,12 @@ Base = declarative_base()
 
 class User(Base):
     """
-    User class
+    User class representing a database table named 'users'
     """
     __tablename__ = 'users'
 
     id: int = Column(Integer, primary_key=True)
-    email: str = Column(String(250), nullable=False)
+    email: str = Column(String(250), nullable=False, unique=True)
     hashed_password: str = Column(String(250), nullable=False)
     session_id: str = Column(String(250), nullable=True)
     reset_token: str = Column(String(250), nullable=True)
