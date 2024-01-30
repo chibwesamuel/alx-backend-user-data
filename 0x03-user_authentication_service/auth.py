2f26e7071074e2a87a714e885c1b70f9f7b1b17e
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 11:16:29 +0200

    Work in progress

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,17 +2,33 @@
 """
 Auth module
 """
-import bcrypt
+from db import DB
+from user import User
+from typing import Union
 
-
-def _hash_password(password: str) -> bytes:
+class Auth:
+    """Auth class to interact with the authentication database.
     """
-    Hashes a password using bcrypt
 
-    Args:
-        password (str): The password to hash
+    def __init__(self):
+        self._db = DB()
 
-    Returns:
-        bytes: The hashed password
-    """
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The newly registered user.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        user = self._db.find_user_by(email=email)
+        if user:
+            raise ValueError(f"User {email} already exists")
+        hashed_password = _hash_password(password)
+        return self._db.add_user(email, hashed_password)
