1ffde47dfb199e2bb5de5eecba612ea09a2cd6b4
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 21:40:51 +0200

    Basic auth class created

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Class that inherits from auth
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """ Basic authentication class
+    Inherits from Auth
+    """
+    pass  # This class is currently empty, but you can add methods specific to Basic Auth here
