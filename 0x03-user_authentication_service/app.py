615cdb20bb2f0817bd443fa75bc258ebad6ca586
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 12:58:21 +0200

    Worked on app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, abort
 from auth import Auth
 
 app: Flask = Flask(__name__)
@@ -22,6 +22,25 @@ def users() -> tuple:
         return jsonify({"email": email, "message": "user created"}), 201
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def sessions() -> tuple:
+    """Login a user
+
+    Returns:
+        tuple: JSON response with login status and message
+    """
+    email: str = request.form.get("email")
+    password: str = request.form.get("password")
+
+    if AUTH.valid_login(email, password):
+        session_id: str = AUTH.create_session(email)
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", session_id)
+        return response, 200
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
