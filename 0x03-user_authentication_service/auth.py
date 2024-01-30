37e4a4fb9baa6223f59345aa2964430d3300373f
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 13:21:38 +0200

    Worked on auth.py some more

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -103,6 +103,14 @@ class Auth:
                 return None
         return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy session for the user.
+
+        Args:
+            user_id (int): The ID of the user.
+        """
+        self._db.update_user(user_id, session_id=None)
+
     def _generate_uuid(self) -> str:
         """Generate a UUID string.
 
