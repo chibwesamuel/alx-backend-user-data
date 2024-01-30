75ab2baacda6f41625a2bfd6a1b8f0dda1dc5905
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 12:02:46 +0200

    One step at a time

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,48 +1,46 @@
 #!/usr/bin/env python3
 
+from typing import Union
+from bcrypt import checkpw
 from db import DB
-from bcrypt import checkpw, gensalt, hashpw
+from user import User
 
 
 class Auth:
-    """Auth class to interact with the authentication database.
+    """
+    Auth class to interact with the authentication database.
     """
 
     def __init__(self) -> None:
         self._db = DB()
 
-    def register_user(self, email: str, password: str) -> None:
-        """Register a new user.
+    def register_user(self, email: str, password: str) -> Union[User, None]:
+        """
+        Register a new user.
 
         Args:
             email (str): The email of the user.
             password (str): The password of the user.
+
+        Returns:
+            Union[User, None]: The newly created User object,
+            or None if user already exists.
         """
         try:
-            self._db.add_user(email, self._hash_password(password))
+            return self._db.add_user(email, password)
         except ValueError:
-            raise ValueError(f"User {email} already exists")
-
-    def _hash_password(self, password: str) -> bytes:
-        """Hash a password.
-
-        Args:
-            password (str): The password to hash.
-
-        Returns:
-            bytes: The hashed password.
-        """
-        return hashpw(password.encode('utf-8'), gensalt())
+            return None
 
     def valid_login(self, email: str, password: str) -> bool:
-        """Check if login credentials are valid.
+        """
+        Validate user login credentials.
 
         Args:
             email (str): The email of the user.
             password (str): The password of the user.
 
         Returns:
-            bool: True if login credentials are valid, False otherwise.
+            bool: True if login is valid, False otherwise.
         """
         try:
             user = self._db.find_user_by(email=email)
