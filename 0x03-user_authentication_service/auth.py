bed8412002e995c29d1d554207c5dc82c0d7de7e
chibwesamuel <smchibwe@yahoo.com>
2024-01-30 12:10:39 +0200

    Surely now

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
 
+import bcrypt
+import uuid
 from typing import Union
-from bcrypt import checkpw
+
 from db import DB
 from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 class Auth:
-    """
-    Auth class to interact with the authentication database.
-    """
+    """Auth class to interact with the authentication database."""
 
-    def __init__(self) -> None:
+    def __init__(self):
         self._db = DB()
 
     def register_user(self, email: str, password: str) -> Union[User, None]:
@@ -27,13 +28,15 @@ class Auth:
             or None if user already exists.
         """
         try:
-            return self._db.add_user(email, password)
-        except ValueError:
-            return None
+            self._db.find_user_by(email=email)
+            raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            hashed_password = self._hash_password(password)
+            return self._db.add_user(email, hashed_password)
 
     def valid_login(self, email: str, password: str) -> bool:
         """
-        Validate user login credentials.
+        Check if login is valid.
 
         Args:
             email (str): The email of the user.
@@ -44,7 +47,29 @@ class Auth:
         """
         try:
             user = self._db.find_user_by(email=email)
-            return checkpw(password.encode('utf-8'),
-                           user.hashed_password.encode('utf-8'))
-        except ValueError:
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password.encode('utf-8'))
+        except NoResultFound:
             return False
+
+    def _hash_password(self, password: str) -> str:
+        """
+        Hash a password.
+
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            str: The hashed password.
+        """
+        salt = bcrypt.gensalt()
+        return bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    def _generate_uuid(self) -> str:
+        """
+        Generate a new UUID.
+
+        Returns:
+            str: A string representation of the generated UUID.
+        """
+        return str(uuid.uuid4())
